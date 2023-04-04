a566309d7faf7adf36fd1e1c821312c908731ef2
동찌 <71123399+kimdongzzi@users.noreply.github.com>
2023-04-04 10:54:40 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -1,7 +1,5 @@
 function solution(absolutes, signs) {
-    let arr = absolutes
-    for(i in arr){
-       signs[i] ? arr[i] : arr[i] = -arr[i]
-    }
-    return arr.reduce((acc, cal) => acc + cal)
+    return absolutes.reduce((acc, val, idx) => 
+                            acc + (val * (signs[idx] ? 1 : -1)), 0
+                           )
 }
