a687a38fdcbac0c235974867be7f6ba1c2bff3fd
동찌 <71123399+kimdongzzi@users.noreply.github.com>
2023-04-05 15:11:48 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,5 +1,3 @@
 function solution(numbers) {
-    var answer = Array(10).fill(0, 0).map((val, idx) => idx)
-    let num = numbers.sort((a,b) => a - b)
-    return answer.filter((val, idx) => !num.includes(val)).reduce((acc, val) => acc + val, 0)
+    return numbers.reduce((acc,val)=> acc - val, 45)
 }
