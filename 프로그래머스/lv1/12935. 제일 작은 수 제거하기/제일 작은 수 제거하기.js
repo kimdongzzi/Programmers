3ecd832e424e15e3f65e6bc1573a5eb0ec90c46d
동찌 <71123399+kimdongzzi@users.noreply.github.com>
2023-04-05 15:03:05 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.09 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,5 +1,4 @@
 function solution(arr) {
     const min = Math.min(...arr)
-    let answer = arr.filter(val => val !== min)
-    return answer.length === 0 ? [-1] : answer
+    return arr.length !== 1 ? arr.filter(val => val !== min) : [-1] 
 }
