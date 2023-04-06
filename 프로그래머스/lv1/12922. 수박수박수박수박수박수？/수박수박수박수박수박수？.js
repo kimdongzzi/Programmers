b6ddfeaa726d12d926704d3ad69bb8bafde9048c
동찌 <71123399+kimdongzzi@users.noreply.github.com>
2023-04-06 11:12:44 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.05 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,7 +1,9 @@
 function solution(n) {
-    let answer = []
-    for(let i = 1; i<=n ; i++){
-        i % 2 === 0 ? answer.push("박") : answer.push("수")
-    }
-    return answer.join("")
+    // let answer = []
+    // for(let i = 1; i<=n ; i++){
+    //     i % 2 === 0 ? answer.push("박") : answer.push("수")
+    // }
+    // return answer.join("")
+    
+    return "수박".repeat(n).slice(0,n)
 }
