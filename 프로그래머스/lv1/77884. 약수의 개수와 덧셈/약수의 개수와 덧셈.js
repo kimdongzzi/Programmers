56b85b79f1e5bd5be2e5199ad68153459e1de8f0
동찌 <71123399+kimdongzzi@users.noreply.github.com>
2023-04-10 11:56:42 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,13 +1,11 @@
 function solution(left, right) {
     let answer = 0;
     for(let i = left; i <= right; i++){
-        let count = 0;
-        for(let j = 1; j <= i; j ++){
-            if(i % j === 0){
-                count += 1
-            }
+        if(Number.isInteger(Math.sqrt(i))){
+            answer -= i
+        }else{
+            answer += i
         }
-        count % 2 === 0 ? answer += i : answer -= i
     }
     return answer
 }
