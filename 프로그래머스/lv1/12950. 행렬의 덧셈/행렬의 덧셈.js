9c38c75030014a407678e94497a30105ed2ebb76
동찌 <71123399+kimdongzzi@users.noreply.github.com>
2023-04-11 11:46:53 +0900

    [level 1] Title: 행렬의 덧셈, Time: 8.44 ms, Memory: 67.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -0,0 +1,10 @@
+function solution(arr1, arr2) {
+    let result = [[]];
+    for(let i = 0; i < arr1.length; i++){
+        result[i] = [];
+        for(let j = 0; j < arr1[i].length; j++){                
+           result[i].push(arr1[i][j] + arr2[i][j])
+        }
+    }
+    return result
+}
