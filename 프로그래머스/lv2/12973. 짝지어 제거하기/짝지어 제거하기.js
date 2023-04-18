c4c732b569e33685ca4161c13638a8a70de872ed
동찌 <71123399+kimdongzzi@users.noreply.github.com>
2023-04-18 14:10:53 +0900

    [level 2] Title: 짝지어 제거하기, Time: 41.09 ms, Memory: 58 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,12 @@
+function solution(s)
+{
+    const stack = []
+    for( let i of s ){
+        stack.push(i)
+        if(stack[stack.length-1] == stack[stack.length-2] ){
+            stack.pop()
+            stack.pop()
+        }
+    }
+    return stack.length === 0 ? 1 : 0
+}
